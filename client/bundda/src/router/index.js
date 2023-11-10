960513707ecafdad4be366a4d4b619337eafb3d3
joo-none <joo-none>
2023-11-11 06:30:14 +0900

    feat:만남의 광장

--- client/bundda/src/router/index.js
+++ client/bundda/src/router/index.js
@@ -16,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/juneon1',
+      name: 'juneon1',
+      component: () => import('../views/juneon1.vue')
     }
   ]
 })
